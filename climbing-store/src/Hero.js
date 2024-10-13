92cb1fff37aca183754a07f99c036197f32ecea5
apierson3 <apierson3@gmail.com>
2024-10-12 22:37:49 -0500

    update ghpages

--- climbing-store/src/Hero.js
+++ climbing-store/src/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
         <div className="hero-blur">
             <video className="hero-video" src={videoSrc} autoPlay loop muted />
             <div className="hero-overlay">
-                <h1>Innovative Climbing</h1>
+                <h1>Innovative Climbing Design</h1>
                 <p>Discover the latest in climbing technology with our cutting-edge gear designed for performance, comfortability, and safety. 
                     From ultra-ergonomic 'quick' personal anchor systems to sustainable climbing swag, our products are crafted to elevate your climbing experience.</p>
                 <button className="hero-button">Start Shopping</button>
