6cda2f154c02a2d83387f981240cc09f4bdc3b16
apierson3 <apierson3@gmail.com>
2024-10-15 22:18:11 -0500

    Contact page

--- climbing-store/src/Contact.js
+++ climbing-store/src/Contact.js
@@ -1,8 +1,31 @@
-// src/Contact.js
 import React from 'react';
+import './Contact.css';
 
 function Contact() {
-  return <h2>Contact Page</h2>;
+  return (
+    <div className="contact-container">
+      <div className="contact-blur">
+        <h1>Contact Us</h1>
+        <section>
+          <h2>Get in Touch</h2>
+          <p>We'd love to hear from you! Whether you have questions about our products, need assistance with an order, or just want to chat about climbing ideas, reach out to us.</p>
+        </section>
+        <section>
+          <h2>Contact Information</h2>
+          <ul>
+            <li><strong>Email:</strong> aptest123mizzou@gmail.com </li>
+            <li><strong>Address:</strong> Kansas City, Missouri</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Follow Us</h2>
+          <ul>
+            <li><strong>Instagram:</strong> pierson_andrew</li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
