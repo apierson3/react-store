091fc4c5bb08f21520571904426b0cdc1bb23d7f
apierson3 <apierson3@gmail.com>
2024-10-20 22:04:09 -0500

    update popup styling

--- climbing-store/src/ConfirmDeleteModal.js
+++ climbing-store/src/ConfirmDeleteModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import './ConfirmDeleteModal.css'; // Ensure the CSS file is imported
 
 Modal.setAppElement('#root'); // Ensure accessibility
 
@@ -14,8 +15,8 @@ const ConfirmDeleteModal = ({ isOpen, onRequestClose, onConfirm }) => {
     >
       <h2>Are you sure you want to delete this post?</h2>
       <div className="modal-buttons">
-        <button className="modal-button" onClick={onConfirm}>Delete</button>
-        <button className="modal-button" onClick={onRequestClose}>Cancel</button>
+        <button className="modal-button modal-button-confirm" onClick={onConfirm}>Delete</button>
+        <button className="modal-button modal-button-cancel" onClick={onRequestClose}>Cancel</button>
       </div>
     </Modal>
   );
