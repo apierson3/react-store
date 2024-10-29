7133c92c759d895223883f0c26955313848f5f24
apierson3 <apierson3@gmail.com>
2024-10-28 23:28:31 -0500

    update text alignment

--- climbing-store/src/BlogPost.js
+++ climbing-store/src/BlogPost.js
@@ -41,8 +41,6 @@ function BlogPost() {
             {post.youtubeLink && (
               <div className="video-responsive">
                 <iframe
-                  width="560"
-                  height="315"
                   src={`https://www.youtube.com/embed/${extractVideoId(post.youtubeLink)}`}
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
